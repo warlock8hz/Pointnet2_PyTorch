66a775f3facbdab1a2ae08da47cfb5d5ab096d68
en1060 <en1060@zbox>
2021-01-19 18:49:41 +0800

    Update evaluate.py evaluation function for predicting single asc file

--- pointnet2/evaluate.py
+++ pointnet2/evaluate.py
@@ -29,7 +29,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-N_GRAM = 180 #196 for 6 GB GRAM
+N_GRAM = 128 #196 for 6 GB GRAM
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = '/home/en1060/Projects/Pointnet2_PyTorch'
 CKPT_DIR = os.path.join(ROOT_DIR, 'CKPT')
@@ -131,7 +131,7 @@ def main(cfg):
 
     if cfg.task_model.name == 'sem-ssg':
         checkpoint_folder = 'sem-ssg'
-        checkpoint_path = os.path.join(CKPT_DIR, checkpoint_folder, 'utility_predict0130.5-6_epoch=46-val_loss=0.62-val_acc=0.874.ckpt')#'eutility_poch=34-val_loss=0.78-val_acc=0.859.ckpt')#'best869.ckpt')#'best39_valve_no_scale.ckpt')
+        checkpoint_path = os.path.join(CKPT_DIR, checkpoint_folder, 'utilityOnly_predict0130.5-6_raw_resolution_epoch=10-val_loss=0.01-val_acc=0.999.ckpt')#'eutility_poch=34-val_loss=0.78-val_acc=0.859.ckpt')#'best869.ckpt')#'best39_valve_no_scale.ckpt')
         fList = []
         #scanH5File(H5_DIR, fList)
         scanH5File('/home/en1060/Desktop/importh5', fList)
@@ -153,7 +153,7 @@ def main(cfg):
     sequenceID = 0
 
     if cfg.task_model.name == 'sem-ssg':
-        if True: # regular check for hdf5 scenes
+        if False: # regular check for hdf5 scenes
             #for item in fList:
             item = "/home/en1060/Desktop/importh5/_1-PTS_CHD0130.5-0130.6.ply"
             print(sequenceID)
@@ -207,8 +207,8 @@ def main(cfg):
             pts2hd5.hd5prediction2txt(pts, labels, origins, item, center)
 
             sequenceID += 1
-        else: # scene/folder-based check
-            path = '/home/en1060/Desktop/importh5/utility-test'
+        elif False: # scene/folder-based check
+            path = '/home/en1060/Desktop/importh5'#/utility-test'
             all_pclist, all_labelvallist, all_gridoriginlist, all_centers, all_filelist = \
                 pts2hd5.asc2hdf5dataSingleScene(path)
             for id_file in range(len(all_pclist)):
@@ -264,6 +264,61 @@ def main(cfg):
                                           os.path.join(path, all_filelist[id_file] + '.pred.txt'), all_centers[id_file])
 
                 sequenceID += 1
+        else:
+            #path = '/home/en1060/Desktop/importh5'#/utility-test'
+            path = '/home/en1060/Desktop/importh5'
+            a_data, D2labelVal_list, a_origins, center = pts2hd5.asc2hdf5data_prediction(path)
+            print(sequenceID)
+            # pts, labels = i3ssLoader._load_data_file(item)
+            # pts, labels, origins, center = pts2hd5.ply2hdf5data(item)
+            group_GRAM = math.floor(len(a_data) / N_GRAM)
+            labels = D2labelVal_list.copy()
+
+            for i_GRAM in range(group_GRAM + 1):
+                correct = 0
+                print('\t%d.%d' % (sequenceID, i_GRAM))
+
+                N_start = i_GRAM * N_GRAM
+                N_end = (i_GRAM + 1) * N_GRAM
+                if i_GRAM == group_GRAM:
+                    N_end = len(a_data)
+
+                pts_GRAM = a_data[N_start:N_end, ...]
+                labels_GRAM = D2labelVal_list[N_start:N_end, ...]
+                # data_batches = np.concatenate(pts, 0) # 3D to 2D
+                # labels_batches = np.concatenate(labels, 0) # 3D to 2D
+
+                # Model inference
+                inputs = {'point_clouds': torch.from_numpy(pts_GRAM).to(device)}
+                tic = time.time()
+                with torch.no_grad():
+                    optimizer.zero_grad()
+                    predicted_labels = torch.from_numpy(
+                        np.random.randint(10, 11, size=(len(pts_GRAM), len(pts_GRAM[0])))).to(device)
+                    res = model.validate_once((inputs['point_clouds'], predicted_labels), None)
+                    predicted_labels = res['log']['label']
+
+                toc = time.time()
+                pl = predicted_labels.cpu().detach().numpy()
+
+                for idx in range(len(pl)):
+                    for idy in range(len(pl[idx])):
+                        if pl[idx][idy] == labels_GRAM[idx][idy]:
+                            correct += 1
+
+                total = pts_GRAM.shape[0] * pts_GRAM.shape[1]
+
+                print(correct / total)
+
+                labels[N_start:N_end, ...] = pl
+
+            # f = h5py.File("/home/en1060/Desktop/importh5/classroom_predict.h5", 'w')
+            # f = h5py.File(item + 'predict.h5', 'w')
+            # data = f.create_dataset("data", data=pts)
+            # pid = f.create_dataset("label", data=labels)
+            pts2hd5.hd5prediction2txt(a_data, labels, a_origins, os.path.join(path, 'res.pred.txt'), center)
+
+            sequenceID += 1
 
     else:#classification
         for item in fList:
