aca76295429a49712787ecddb1786dd349680c90
en1060 <en1060@zbox>
2021-01-23 01:01:39 +0800

    Update pts2hd5.py for updated utility classes only

--- pointnet2/data/pts2hd5.py
+++ pointnet2/data/pts2hd5.py
@@ -19,91 +19,111 @@ GRID_DIMENTION = [1.0, 1.0]
 GRID_SAMPLE_NUM = 4096
 
 def returnIndoor3DLabels(label):
+    # if label == 0:
+    #     return 'ceiling'
+    # elif label == 1:
+    #     return 'floor'
+    # elif label == 2:
+    #     return 'wall'
+    # elif label == 3:
+    #     return 'column'
+    # elif label == 4:
+    #     return 'beam'
+    # elif label == 5:
+    #     return 'window'
+    # elif label == 6:
+    #     return 'door'
+    # elif label == 7:
+    #     return 'table'
+    # elif label == 8:
+    #     return 'chair'
+    # elif label == 9:
+    #     return 'bookcase'
+    # elif label == 10:
+    #     return 'sofa'
+    # elif label == 11:
+    #     return 'board'
+    # elif label == 12:
+    #     return 'clutter'
     if label == 0:
-        return 'ceiling'
-    elif label == 1:
-        return 'floor'
-    elif label == 2:
-        return 'wall'
-    elif label == 3:
-        return 'column'
-    elif label == 4:
-        return 'beam'
-    elif label == 5:
-        return 'window'
-    elif label == 6:
-        return 'door'
-    elif label == 7:
-        return 'table'
-    elif label == 8:
-        return 'chair'
-    elif label == 9:
-        return 'bookcase'
-    elif label == 10:
-        return 'sofa'
-    elif label == 11:
-        return 'board'
-    elif label == 12:
-        return 'clutter'
-    elif label == 13:
         return 'pipe'
-    elif label == 14:
+    elif label == 1:
         return 'bend-box'
-    elif label == 15:
+    elif label == 2:
         return 'valve'
-    elif label == 16:
+    elif label == 3:
         return '90d-elbow'
-    elif label == 17:
+    elif label == 4:
         return '22.5d-elbow'
-    elif label == 18:
+    elif label == 5:
         return '45d-elbow'
-    elif label == 19:
+    elif label == 6:
         return 'tee'
+    elif label == 7:
+        return 'fence'
+    elif label == 8:
+        return 'ladder'
+    elif label == 9:
+        return 'ground'
+    elif label == 10:
+        return 'framework'
+    elif label == 11:
+        return 'barrier'
     else: # should not happen
         return 'unknownError'
     return 'unknownError' # if error
 
 def returnIndoor3DLabelIds(strName):
-    if strName.lower() == 'ceiling':
+    # if strName.lower() == 'ceiling':
+    #     return 0
+    # elif strName.lower() == 'floor':
+    #     return 1
+    # elif strName.lower() == 'wall':
+    #     return 2
+    # elif strName.lower() == 'column':
+    #     return 3
+    # elif strName.lower() == 'beam':
+    #     return 4
+    # elif strName.lower() == 'window':
+    #     return 5
+    # elif strName.lower() == 'door':
+    #     return 6
+    # elif strName.lower() == 'table':
+    #     return 7
+    # elif strName.lower() == 'chair':
+    #     return 8
+    # elif strName.lower() == 'bookcase':
+    #     return 9
+    # elif strName.lower() == 'sofa':
+    #     return 10
+    # elif strName.lower() == 'board':
+    #     return 11
+    # elif strName.lower() == 'clutter':
+    #     return 12
+    if strName.lower() == 'pipe':
         return 0
-    elif strName.lower() == 'floor':
-        return 1
-    elif strName.lower() == 'wall':
-        return 2
-    elif strName.lower() == 'column':
-        return 3
-    elif strName.lower() == 'beam':
-        return 4
-    elif strName.lower() == 'window':
-        return 5
-    elif strName.lower() == 'door':
-        return 6
-    elif strName.lower() == 'table':
-        return 7
-    elif strName.lower() == 'chair':
-        return 8
-    elif strName.lower() == 'bookcase':
-        return 9
-    elif strName.lower() == 'sofa':
-        return 10
-    elif strName.lower() == 'board':
-        return 11
-    elif strName.lower() == 'clutter':
-        return 12
-    elif strName.lower() == 'pipe':
-        return 13
     elif strName.lower() == 'bend-box':
-        return 14
+        return 1
     elif strName.lower() == 'valve':
-        return 15
+        return 2
     elif strName.lower() == '90d-elbow':
-        return 16
+        return 3
     elif strName.lower() == '22.5d-elbow':
-        return 17
+        return 4
     elif strName.lower() == '45d-elbow':
-        return 18
+        return 5
     elif strName.lower() == 'tee':
-        return 19
+        return 6
+    elif strName.lower() == 'fence':
+        return 7
+    elif strName.lower() == 'ladder':
+        return 8
+    elif strName.lower() == 'ground':
+        return 9
+    elif strName.lower() == 'framework':
+        return 10
+    elif strName.lower() == 'barrier':
+        return 11
     else: # should not happen
         return 255
     return 255#should be error
